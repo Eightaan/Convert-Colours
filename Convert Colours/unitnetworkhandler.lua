9de09d9d50c1e6a8986284a140f64b06c95f4755
Eightaan <57832730+Eightaan@users.noreply.github.com>
2020-01-09 14:21:15 +0100

    old crap

--- Convert Colours/unitnetworkhandler.lua
+++ Convert Colours/unitnetworkhandler.lua
@@ -1,25 +1,19 @@
 local mark_minion = UnitNetworkHandler.mark_minion
 
 function UnitNetworkHandler:mark_minion(unit, minion_owner_peer_id, convert_enemies_health_multiplier_level, passive_convert_enemies_health_multiplier_level, sender, owner)
-	mark_minion(self, unit, minion_owner_peer_id, convert_enemies_health_multiplier_level, passive_convert_enemies_health_multiplier_level, sender)
-		
-	    if alive(unit) and unit:in_slot(16) and owner == managers.network:session():local_peer():id() then -- replace jokers
-		    table.insert(GroupAIStateBase.PLAYER_JOKERS, unit) 
-		end		
-		
-		if unit then
-			local color_id = minion_owner_peer_id and managers.network:session():peer(minion_owner_peer_id).unit and managers.criminals:character_color_id_by_unit(managers.network:session():peer(minion_owner_peer_id):unit()) or 1
-				unit:contour():add("joker", nil, 1)
-				unit:contour():change_color("joker", tweak_data.peer_vector_colors[color_id])		
-				
-			unit:base().owner_peer_id = minion_owner_peer_id
-		end
+mark_minion(self, unit, minion_owner_peer_id, convert_enemies_health_multiplier_level, passive_convert_enemies_health_multiplier_level, sender)	
+	if unit then
+		local color_id = minion_owner_peer_id and managers.network:session():peer(minion_owner_peer_id).unit and managers.criminals:character_color_id_by_unit(managers.network:session():peer(minion_owner_peer_id):unit()) or 1
+			unit:contour():add("joker", nil, 1)
+			unit:contour():change_color("joker", tweak_data.peer_vector_colors[color_id])		
+		unit:base().owner_peer_id = minion_owner_peer_id
+	end
 end
 	
-	local hostage_trade = UnitNetworkHandler.hostage_trade
-	function UnitNetworkHandler:hostage_trade(unit, enable, trade_success, skip_hint)
-		if unit then
-			unit:contour():remove("joker")
-		end
-		hostage_trade(self, unit, enable, trade_success, skip_hint)
+local hostage_trade = UnitNetworkHandler.hostage_trade
+function UnitNetworkHandler:hostage_trade(unit, enable, trade_success, skip_hint)
+	if unit then
+		unit:contour():remove("joker")
 	end
+	hostage_trade(self, unit, enable, trade_success, skip_hint)
+end
